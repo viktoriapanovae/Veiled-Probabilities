7d875e84586e4047bacebe33ff5116737d642148
viktoriapanovae <pppifvc508@outlook.com>
2025-12-24 13:42:55 +0800

    Wire deploy script for VeiledPredictionMarket

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedMarket = await deploy("VeiledPredictionMarket", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`VeiledPredictionMarket contract: `, deployedMarket.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_veiledPredictionMarket"; // id required to prevent reexecution
+func.tags = ["VeiledPredictionMarket"];
